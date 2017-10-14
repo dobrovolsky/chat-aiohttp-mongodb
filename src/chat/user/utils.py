eea43fe4c9d33712fa3eb2cff40a24e16efa6d11
Bogdan Dobrovolsky <bogdan.gm24@gmail.com>
2017-10-14 22:32:50 +0300

    refactor sign up * add base validator * add password two times * move validation from user model class * add ability to set password

--- src/chat/user/utils.py
+++ src/chat/user/utils.py
@@ -1,4 +1,5 @@
 import hashlib
+from marshmallow.exceptions import ValidationError
 from motor import motor_asyncio
 
 from chat.config import settings
@@ -13,10 +14,28 @@ def get_hash(pswd):
     return password
 
 
-def message_collection():
+def get_message_collection():
     """Return connection to user collection"""
     client = motor_asyncio.AsyncIOMotorClient(settings.MONGO_HOST, settings.MONGO_PORT, io_loop=loop)
     db = client[settings.MONGO_DB_NAME]
     collection = db[settings.MONGO_USER_COLLECTION]
     return collection
 
+
+def basic_string_validation(value, min_length=None, max_length=None, blank=False):
+    """Provide basic validation for string"""
+    if not isinstance(value, str):
+        raise ValidationError('should be a string')
+    if min_length and len(value) < min_length:
+        raise ValidationError(f'string should be longer than {min_length}')
+    if max_length and len(value) > max_length:
+        raise ValidationError(f'string should be shorter than {max_length}')
+    if not blank and not value:
+        raise ValidationError(f'string should not be blank')
+
+
+async def is_email_exists_in_db(email) -> bool:
+    """Check email in db"""
+    collection = get_message_collection()
+    data = await collection.find_one({'email': email})
+    return bool(data)
